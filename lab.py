07cfdc03f444467723e93c2b57cb6258bbaa4c9b
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-03-19 12:19:40 +0700

    update

--- lab.py
+++ lab.py
@@ -2,23 +2,7 @@
 Nama : Said Muhammad Mazaya
 NIM : 221402129
 
-Soal 2 = Write a class that calculates and stores the height and weight of a person in metric. 
-    The file should be named lab.py.  BMI is calculated using this formula:
-    
-    Weight/Height^2 - weight is in kilograms and height is in meters.
-
-The class should have two properties named:
-
-- Weight
-
-- Height
-
-The class should have two methods:
-
-- BMI_Value = This takes no arguments and returns a decimal value of the BMI
-
-- Equals = This should override the equals method from the object class to compare the weight and height of two BMI objects.  
-    To override the equal method you should implement this method: __eq__(self, other) and return a boolean
+Soal 2 = Write a class that calculates and stores the height and weight of a person in metric. The file should be named lab.py.  
 """
 
 class BMI:
